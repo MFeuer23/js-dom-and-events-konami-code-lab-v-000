a6daec3873b7f0e88d955a1a7feaa44047afb037
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 14:42:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -24,21 +24,3 @@ let index = 0;
   
 document.body.addEventListener('keydown', function init(e));
 
-
-function init(e) {
-    const key = parseInt(e.detail || e.which);
- 
-    if (key === code[index]) {
-      index++;
- 
-      if (index === code.length) {
-        alert("Hurray!");
- 
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
-  
-document.body.addEventListener('keydown', init); //place in function init; addEventListener will take an anonymous callback function where e, the event, is passed in as an argument. So you just need to refactor.
