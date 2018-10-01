61262d63747f9f12067e202276ce69baa5ff6355
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 13:47:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,19 @@ let index = 0;
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-alert('keydown!')
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
 
 document.body.addEventListener('keydown', init());
