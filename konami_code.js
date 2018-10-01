3c886c59b1e1138c253ff8173eb60aa4e1716669
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 14:44:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,5 +22,19 @@ let index = 0;
     }
   }
   
-document.body.addEventListener('keydown', init(e));
+document.body.addEventListener('keydown', function init(e) {
+    const key = parseInt(e.detail || e.which);
+ 
+    if (key === code[index]) {
+      index++;
+ 
+      if (index === code.length) {
+        alert("Hurray!");
+ 
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 
