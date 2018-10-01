e9469294636bbff3d6113d1351b4460c90a699b9
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 13:58:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -21,3 +21,14 @@ function init(e) {
 }
 
 document.body.querySelector('input').addEventListener('keydown', init());
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
+});
